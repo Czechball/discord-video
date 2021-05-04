76dae177b67e396d9b0fef29a934bfc1e974d1e5
Czechball <david@jenne.cz>
2021-05-05 01:39:17 +0200

    added audio bitrate calculation

--- discord-video.sh
+++ discord-video.sh
@@ -2,7 +2,8 @@
 
 # 64 000 000 bits / video length = target bitrate
 
-MAX_SIZE="64000000"
+MAX_VIDEO_SIZE="60000000"
+MAX_AUDIO_SIZE="4000000"
 
 # Check argument
 
@@ -14,9 +15,10 @@ fi
 
 # Check if file is video, get duration if it is
 
-DURATION=$(ffprobe -hide_banner "$1" -show_entries format=duration -v quiet -of csv="p=0" || echo Error: "$1" is not a video file; exit)
+DURATION=$(ffprobe -hide_banner "$1" -show_entries format=duration -v quiet -of csv="p=0")
 
 if [[ "$DURATION" == "" ]]; then
+	echo Error, ffprobe returned no duration. "$1" is possibly not a video file
 	exit
 fi
 
@@ -25,8 +27,7 @@ echo "$1" is a video file and is $DURATION seconds long
 # Calculate bitrate
 
 ADJUSTED_DURATION=$(printf "%.0f\n" "$DURATION")
-BITRATE=$(echo $((MAX_SIZE / ADJUSTED_DURATION*75/100)))
+VIDEO_BITRATE=$(echo $((MAX_VIDEO_SIZE / ADJUSTED_DURATION*75/100)))
+AUDIO_BITRATE=$(echo $((MAX_AUDIO_SIZE / ADJUSTED_DURATION*90/100)))
 
-echo video should have a bitrate of $(((MAX_SIZE / ADJUSTED_DURATION)/1000)) kbps
-
-ffmpeg -i "$1" -c:v libvpx-vp9 -b:v "$BITRATE" -vf scale=1280:720 -c:a libopus -b:a 96K "$1-compressed.webm"
+ffmpeg -i "$1" -c:v libvpx-vp9 -b:v "$VIDEO_BITRATE" -vf scale=1280:720 -c:a libopus -b:a "$AUDIO_BITRATE" "$1-compressed.webm"
