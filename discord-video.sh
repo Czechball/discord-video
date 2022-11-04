3d4010bc6d1aac853837ab322f044377e664947c
Czechball <13371865+Czechball@users.noreply.github.com>
2022-11-04 10:13:41 +0100

    fixed broken variable and other fixes

--- discord-video.sh
+++ discord-video.sh
@@ -3,7 +3,7 @@
 # 64 000 000 bits / video length = target bitrate
 
 MAX_VIDEO_SIZE="60000000"
-MAX_AUDIO_SIZE="4000000"
+#MAX_AUDIO_SIZE="4000000"
 
 # Check argument
 
@@ -22,12 +22,12 @@ if [[ "$DURATION" == "" ]]; then
 	exit
 fi
 
-echo "$1" is a video file and is $DURATION seconds long
+echo "$1" is a video file and is "$DURATION" seconds long
 
 # Calculate bitrate
 
 ADJUSTED_DURATION=$(printf "%.0f\n" "$DURATION")
-VIDEO_BITRATE=$(echo $((MAX_VIDEO_SIZE / ADJUSTED_DURATION)))
-AUDIO_BITRATE=$(echo $((MAX_AUDIO_SIZE / ADJUSTED_DURATION)))
+VIDEO_BITRATE=$((MAX_VIDEO_SIZE / ADJUSTED_DURATION))
+#AUDIO_BITRATE=$(echo $((MAX_AUDIO_SIZE / ADJUSTED_DURATION)))
 
-ffmpeg -hide_banner -i "$1" -c:v libvpx-vp9 -row-mt 1 -b:v "$VIDEO_BITRATE" -pix_fmt yuv420p -vf scale=1280:720 -pass 1 -an -f null /dev/null && ffmpeg -hide_banner -i "$1" -c:v libvpx-vp9 -cpu-used 3 -row-mt 1 -b:v "%VIDEO_BITRATE%" -pix_fmt yuv420p -vf scale=1280:720 -pass 2 "$1-compressed.mp4"
+ffmpeg -hide_banner -i "$1" -c:v libvpx-vp9 -row-mt 1 -b:v "$VIDEO_BITRATE" -pix_fmt yuv420p -vf scale=1280:720 -pass 1 -an -f null /dev/null && ffmpeg -hide_banner -i "$1" -c:v libvpx-vp9 -cpu-used 3 -row-mt 1 -b:v "$VIDEO_BITRATE" -pix_fmt yuv420p -vf scale=1280:720 -pass 2 "$1-compressed.mp4"
